982a3161ddb34ef9a65a3c32a2fd3b140ae5b6d4
Leuchtstift <dietrich.leonard@googlemail.com>
2020-04-04 20:53:56 +0200

    added some sprites

--- Assets/Scripts/Ball2.cs
+++ Assets/Scripts/Ball2.cs
@@ -9,31 +9,31 @@ public class Ball2 : MonoBehaviour{
 	float spin = 0.0f;
 	float speed = 0.1f;
 
-	  void FixedUpdate(){
-        if (launched == true) {
-        	transform.position = new Vector3(
-						transform.position.x + direction.x * speed,
-						transform.position.y + direction.y * speed,
-						0f
-					);
-        }
-  }
+	void FixedUpdate(){
+		if (launched == true) {
+			transform.position = new Vector3(
+				transform.position.x + direction.x * speed,
+				transform.position.y + direction.y * speed,
+				0f
+			);
+		}
+	}
 
-  public void spawn(Vector3 pTf) {
-  	gameObject.SetActive(true);
-  	transform.position = new Vector3(
+	public void spawn(Vector3 pTf) {
+		gameObject.SetActive(true);
+		transform.position = new Vector3(
 			pTf.x,
 			pTf.y + 0.4f,
 			0f
 		);
 		launched = false;
-  }
+	}
 
-  public void launch(float pSpeed, float pMaxSpeed) {
-  	direction = new Vector2(
-  		pSpeed/pMaxSpeed,
-  		(pMaxSpeed - Mathf.Abs(pSpeed))/pMaxSpeed
-  	).normalized;
-  	launched = true;
-  }
+	public void launch(float pSpeed, float pMaxSpeed) {
+		direction = new Vector2(
+			pSpeed/pMaxSpeed,
+			(pMaxSpeed - Mathf.Abs(pSpeed))/pMaxSpeed
+		).normalized;
+		launched = true;
+	}
 }
