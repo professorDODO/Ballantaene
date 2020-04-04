99f343f46ad48e86d0e2ba3b6411d9f2ada285c6
professorDODO <roman.pollei@gmail.com>
2020-04-04 20:08:26 +0200

    Ball Skript neue geschrieben; Playerskript umgeschrieben: Mauseingabe als input; ToDo akut: Mittelwert des speeds richtig berechnen

---
+++ Assets/Scripts/Ball2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball2 : MonoBehaviour{
+
+	bool launched = false;
+	Vector2 direction = Vector2.zero;
+	float spin = 0.0f;
+	float speed = 0.1f;
+
+	  void FixedUpdate(){
+        if (launched == true) {
+        	transform.position = new Vector3(
+						transform.position.x + direction.x * speed,
+						transform.position.y + direction.y * speed,
+						0f
+					);
+        }
+  }
+
+  public void spawn(Vector3 pTf) {
+  	gameObject.SetActive(true);
+  	transform.position = new Vector3(
+			pTf.x,
+			pTf.y + 0.4f,
+			0f
+		);
+		launched = false;
+  }
+
+  public void launch(float pSpeed, float pMaxSpeed) {
+  	direction = new Vector2(
+  		pSpeed/pMaxSpeed,
+  		(pMaxSpeed - Mathf.Abs(pSpeed))/pMaxSpeed
+  	).normalized;
+  	launched = true;
+  }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,105 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	float iX;
+	//Script in Editor hinzugefügt
 	[SerializeField]
-	float speed = 0.2f;
+	Ball2 BallScript;
+	
+	Vector3 mousePos = Vector3.zero;
+	Vector3 lastPos = Vector3.zero;
+	int frameCounter = 0;
+	[SerializeField] int speedFrameInterp;
+	[SerializeField] float speed;
+	[SerializeField] float maxSpeed;
+	[SerializeField] float deadZoneSpeed;
+	public float speedMedian = 0f;
+	int spMedCounter = 0;
 
-	void Start () {
+	void Start() {
+		mousePos = posOnScreen();
 		transform.position = new Vector3(0, -4.5f, 0);
+		lastPos = transform.position;
 	}
 
-	void Update () {
-		iX = Input.GetAxis("Horizontal");
+	//LEO: Verstehst du, warum der Mittelwert des Speeds "speedMeadian"
+	//		 Nicht dem tatsächlichem Mittelwert zu entsprechen scheint?
+	void Update() {
+		mousePos = posOnScreen();
+		if (Input.GetMouseButtonDown(0)) {
+			BallScript.spawn(transform.position);
+		}
+		if (Input.GetMouseButton(0)) {
+			if (speedMedian*speed >= 0) {
+				if (Mathf.Abs(speed) > deadZoneSpeed) {
+					speedMedian = (speedMedian + speed)/(1 + spMedCounter);
+					spMedCounter = spMedCounter + 1;
+				}
+			} else {
+				speedMedian = speed;
+				spMedCounter = 0;
+			}
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			BallScript.launch(speedMedian, maxSpeed);
+			speedMedian = 0f;
+			spMedCounter = 0;
+		}
 		
 	}
 
-	void FixedUpdate () {
-		transform.position = new Vector3(transform.position.x + iX * speed, transform.position.y, transform.position.z);
+	void FixedUpdate() {
+		playerPos();
+		//calcSpeed();
+		if (Input.GetMouseButton(0)) {
+			//ToDo: Energieverlust im Anspin-modus
+		}
+	}
+
+
+	Vector3 posOnScreen() {
+		return Camera.main.ScreenToWorldPoint(new Vector3(
+			Input.mousePosition.x,
+			Input.mousePosition.y,
+			Camera.main.nearClipPlane
+		));
+	}
+
+	void playerPos() {
+		float nextSpeed = (mousePos.x - transform.position.x)/Time.fixedDeltaTime;
+		if (Mathf.Abs(nextSpeed) > maxSpeed) {
+			transform.position = new Vector3(
+				transform.position.x + Mathf.Abs(nextSpeed)/nextSpeed*maxSpeed*Time.fixedDeltaTime,
+				transform.position.y,
+				transform.position.z
+			);
+			speed = Mathf.Abs(nextSpeed)/nextSpeed*maxSpeed;
+		}else {
+			transform.position = new Vector3(
+				mousePos.x,
+				transform.position.y,
+				transform.position.z
+			);
+			speed = nextSpeed;
+		}
+		//ToDo: Hardcoded gegen Wandpos ersetzen
 		if (transform.position.x > 3.4f) {
 			transform.position = new Vector3(3.4f, -4.5f, 0);
 		} else if (transform.position.x < -3.4f) {
 			transform.position = new Vector3(-3.4f, -4.5f, 0);
 		}
 	}
+
+	//momentan obsolet, könnte aber später noch hilfreich sein, wenn der Speed nicht akurat genug berechnet wird
+	float calcSpeed() {
+		float tempSpeed = (transform.position.x - lastPos.x)/(speedFrameInterp*Time.fixedDeltaTime);
+		if (frameCounter >= speedFrameInterp) {
+			lastPos = transform.position;
+			frameCounter = 0;
+		} else {
+			frameCounter = frameCounter + 1;
+		}
+		return tempSpeed;
+	}
 }
+
+
