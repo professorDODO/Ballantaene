0f86c4f324e28800acbffe93485790887af495f1
professorDODO <roman.pollei@gmail.com>
2020-04-08 02:12:12 +0200

    SoundFX für (Ball x Player) hinzugefügt

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour {
 
+	public SoundFX SFX;
 	[SerializeField] Vector2 normal = Vector2.zero;
+	[SerializeField] bool canRunAudio = true;
+	int counter = 0;
 
 	private void OnTriggerEnter2D (Collider2D col) {
 		GameObject other = col.gameObject;
 		if (other.CompareTag("Ball")) {
       other.GetComponent<Ball>().reflect(normal);
+      if (gameObject.CompareTag("Player") && canRunAudio) {
+        if (counter == 0) {
+        	SFX.GetComponent<SoundFX>().sfxBounce(0);
+        	counter = 1;
+        } else {
+					SFX.GetComponent<SoundFX>().sfxBounce(1);
+					counter = 0;
+        }
+      }
     }
     if (other.CompareTag("Player")) {
     	other.GetComponent<Player>().setAtWall(true, normal.x);
@@ -20,6 +32,9 @@ public class Collision : MonoBehaviour {
 		GameObject other = col.gameObject;
 		if (other.CompareTag("Player")) {
     	other.GetComponent<Player>().setAtWall(false, 0);
+    }
+    if (other.CompareTag("Ball") && gameObject.CompareTag("Player") && canRunAudio) {
+        canRunAudio = true;
     }
 	}
 }
---
+++ Assets/Scripts/SoundFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFX : MonoBehaviour
+{
+	AudioSource audioSource;
+	[SerializeField] AudioClip[] audioSrcArr;
+
+	void Start() {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+	public void sfxBounce(int i) {
+		audioSource.clip = audioSrcArr[i];
+		audioSource.Play();
+	}
+}
