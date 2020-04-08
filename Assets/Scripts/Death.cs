b7c48375869784b91c965a92d5fdd8da0f65aaaf
professorDODO <roman.pollei@gmail.com>
2020-04-08 21:17:14 +0200

    SoundFX für (Ball Death) hinzugefügt

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Death : MonoBehaviour {
-	private void OnTriggerEnter2D (Collider2D col) {
+
+	AudioSource audioSource;
+
+	void Start() {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+	void OnTriggerEnter2D (Collider2D col) {
 		GameObject other = col.gameObject;
 		if (other.CompareTag("Ball")) {
+			audioSource.Play();
       other.SetActive(false);
     }
 	}
